fd79b1c05266e00b1c8dad4f0906de06cc61fb74
yojan khuble <85030838+yojan15@users.noreply.github.com>
2024-01-09 11:35:36 +0530

    app that take firstname and lastname

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.example.myfirstapp
-
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
-
+import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {
-            Toast.makeText(this,"Button was clicked",Toast.LENGTH_LONG).show()
+
+        findViewById<Button>(R.id.button)?.setOnClickListener {
+           val greeting = findViewById<TextView>(R.id.textView)
+            val firstName = findViewById<EditText>(R.id.editTextText3)?.text.toString().trim()
+            val lastName = findViewById<EditText>(R.id.editTextText1)?.text.toString().trim()
+            if(firstName.isNotEmpty() && lastName.isNotEmpty()) {
+                val nameToDisplay = firstName.plus(" ").plus(lastName)
+                greeting.text = "${getString(R.string.Welcome_To_The_App)} $nameToDisplay"
+        }else {
+            Toast.makeText(this,getString(R.string.Please_Enter_Name),Toast.LENGTH_LONG).show()
+
+        }
+
         }
     }
 }
